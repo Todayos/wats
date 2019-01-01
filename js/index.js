e11c2f0eb56505b5eb96fe7d676eb493f15b2207
wangzedong <734327367@qq.com>
2019-01-01 15:36:49 +0800

    增加位置选择

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-var location = document.getElementsByClassName('city');
-if(MouseEvent==true){
-    location.style.display="inline-block";
-}
+
+//位置选择
+var city = document.getElementsByClassName('city');
+var locationIs = document.getElementsByClassName('location');
+var locationImg = document.getElementsByClassName('location-img');
+locationImg[0].onmouseover = function () {
+    locationIs[0].style.display = 'block';
+};
+locationImg[0].onmouseout = function () {
+    locationIs[0].style.display = 'none';
+};
+locationImg[0].onclick = function () {
+    document.getElementsByClassName('location')[0].style.display = 'none';
+    if (document.getElementsByClassName('city')[0].style.display == 'none') {
+        console.log(document.getElementsByClassName('city')[0].style.display)
+        document.getElementsByClassName('city')[0].style.display = 'block';
+    } else {
+        document.getElementsByClassName('city')[0].style.display = 'none';
+        console.log(document.getElementsByClassName('city')[0].style.display)
+    }
+};
+
+
+//轮播图
