aad560ae2da16e8bd596cfbd47fe97f1fdfdc937
wangzd <734327367@qq.com>
2019-01-03 15:40:17 +0800

    轮播代码补充好了

--- js/index.js
+++ js/index.js
@@ -41,34 +41,37 @@ function accountLoginFnc() {
 }
 
 //轮播图----------------------------------------------------------------------------
-//轮播图下方小圆圈按钮
-function imgBtns(imgClassName, btnClassName) {
-    if (document.getElementsByClassName(imgClassName)[0].style.display == 'none' || document.getElementsByClassName(imgClassName)[0].style.display == '') {
-        document.getElementsByClassName('img1')[0].style.display = 'none';
-        document.getElementsByClassName('img2')[0].style.display = 'none';
-        document.getElementsByClassName('img3')[0].style.display = 'none';
-        document.getElementsByClassName('img4')[0].style.display = 'none';
-        document.getElementsByClassName('btn1')[0].style.backgroundColor = '#f3f3f3';
-        document.getElementsByClassName('btn2')[0].style.backgroundColor = '#f3f3f3';
-        document.getElementsByClassName('btn3')[0].style.backgroundColor = '#f3f3f3';
-        document.getElementsByClassName('btn4')[0].style.backgroundColor = '#f3f3f3';
-        document.getElementsByClassName(imgClassName)[0].style.display = 'block';
-        document.getElementsByClassName(btnClassName)[0].style.backgroundColor = '#e8f808';
-    }
-}
-//轮播图定时变化
-var ctn = 2;
-//改变图片的显示状态
-function imgChange() {
-    var imgCtns = 'img' + ctn;
-    var btnCtns = 'btn' + ctn;
-    imgBtns(imgCtns, btnCtns);
-    console.log("当前展示" + ctn);
+//控制显示的是哪个小圆点和哪张图片
+var ctn = 1;
+//设置定时切换图片
+
+function run() {
+    //让ctn号元素显示，让兄弟元素隐藏
+    $(".wheel img").eq(ctn).fadeIn(500).siblings('img').fadeOut(500);
+    //让ctn号元素变黄色，让兄弟元素颜色变灰
+    $(".btn li").eq(ctn).css('backgroundColor', '#e8f808').siblings('li').css("backgroundColor", "#ddd");
     ctn++;
-    if (ctn == 5) {
-        ctn = 1;
+    if (ctn == 4) {
+        ctn = 0;
     }
 }
-//设置定时效果
-setInterval(imgChange, 4000);
+//设置定时器
+var timer = setInterval(run, 4000);
+
+//鼠标移入wheel div停止定时器，移出重启定时器
+$(".wheel").hover(function () {
+    clearInterval(timer);
+}, function () {
+    timer = setInterval(run, 2000);
+});
+
+$('wheel ul li').click()
+
+//移入下方的小圆圈显示对应的图片
+$(".wheel ul li").mouseenter(function () {
+    ctn = $(this).index();
+    $(".wheel img").eq(ctn).show().siblings('img').hide();
+    //让ctn号元素变黄色，让兄弟元素颜色变灰
+    $(".btn li").eq(ctn).css('backgroundColor', '#e8f808').siblings('li').css("backgroundColor", "#ddd");
+})
 //----------------------------------------------------------------------------
